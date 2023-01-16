ac977c50469b659cbe267a01e49d5846eab8f25b
manu <emanuelepires2000@gmail.com>
2023-01-16 14:20:44 -0300

    Adicionando estilizações.

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,9 +1,26 @@
 import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState("");
   return (
     <main>
-      <nav className={styles.menu}></nav>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__texto}>A casa do código e da massa</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros />
+        </div>
+      </section>
     </main>
   );
 }
